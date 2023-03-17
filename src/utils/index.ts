5ffd9243ef80707ccdc288c71f41608a48a8b461
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-03-17 11:14:30 -0400

    tolerate missing strings

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@
  * Convert a string `fooBAR baz_160054''"1]"` into a slug: `foobar-baz-1600541`
  */
 export const stringToSlug = (text: string) =>
-    text
+    (text || '')
         .trim()
         .toLowerCase()
         .replace(/[\-_+ ]/g, '-')
